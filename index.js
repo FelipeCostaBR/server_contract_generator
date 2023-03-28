49bb2a0ba4b3d7cca02869c11b5b0391916a8da9
Felipe <felipejsilvacosta@gmail.com>
2023-03-28 22:16:45 +1100

    fix img path

--- index.js
+++ index.js
@@ -3,19 +3,15 @@ import fs from 'fs';
 import cors from 'cors'
 import pdf from 'html-pdf';
 import moment from 'moment';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json())
 app.use(cors())
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-
-app.get('/home', (req, res) => {
-  res.json({ result: 'IT IS OKAY' })
-})
 
 app.post('/generate-pdf', (req, res) => {
   const { name,
@@ -28,12 +24,12 @@ app.post('/generate-pdf', (req, res) => {
     rentEndDate } = req.body;
   const today = moment().format('DD/MM/YYYY');
 
-  const base64Image3 = fs.readFileSync('img/image3.png', { encoding: 'base64' });
-  const base64Image2 = fs.readFileSync('img/image2.png', { encoding: 'base64' });
-  const base64Image1 = fs.readFileSync('img/image1.png', { encoding: 'base64' });
+  const base64Image3 = fs.readFileSync(path.join(__dirname, 'img/image3.png'), { encoding: 'base64' });
+  const base64Image2 = fs.readFileSync(path.join(__dirname, 'img/image2.png'), { encoding: 'base64' });
+  const base64Image1 = fs.readFileSync(path.join(__dirname, 'img/image1.png'), { encoding: 'base64' });
 
   // Read the image file and convert it to a base64 string
-  const html_template = fs.readFileSync('./templates/Paymentvehicleconditions.html', 'utf8');
+  const html_template = fs.readFileSync(path.join(__dirname, './templates/Paymentvehicleconditions.html'), 'utf8');
   const replacedHtml = html_template
     .replace(/\{\{name\}\}/g, name)
     .replace(/\{\{value\}\}/g, value)
@@ -51,12 +47,12 @@ app.post('/generate-pdf', (req, res) => {
   pdf.create(replacedHtml).toStream((err, stream) => {
     if (err) return res.sendStatus(500);
     res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename="output.pdf"');
+    res.setHeader('Content-Disposition', `attachment; filename="terms_conditions_${name}_${today}.pdf"`);
     stream.pipe(res);
   });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3030;
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
 });
